2bbf8148a30a8fde443412b9550f07e0fd42d1e0
Eder De Souza <esouza@student.42.fr>
2021-10-08 04:54:18 +0200

    Create user dto

---
+++ api/src/dto/users/put.user.dto.ts
@@ -0,0 +1,12 @@
+export interface PutUserDto {
+  id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  gender: string;
+  sexual_orientation: string;
+  biography: string;
+  tags: string;
+}
