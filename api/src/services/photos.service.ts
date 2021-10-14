2391bffc919ee8426d6d97becc96a2dadba355ee
Eder De Souza <esouza@student.42.fr>
2021-10-14 08:52:17 +0200

    Create photo services

---
+++ api/src/services/photos.service.ts
@@ -0,0 +1,14 @@
+import multer from 'multer';
+import { CRUD } from '../common/interfaces/crud.interface';
+
+class PhotoService implements CRUD {
+  async create() {}
+
+  async list() {}
+
+  async getById() {}
+
+  async patchById() {}
+
+  async delete() {}
+}
