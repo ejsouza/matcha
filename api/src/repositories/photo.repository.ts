328cb071e3ff906941a6ab50ec2e756f1fe48c4a
Eder De Souza <esouza@student.42.fr>
2021-10-17 16:07:06 +0200

    Create photo repository

---
+++ api/src/repositories/photo.repository.ts
@@ -0,0 +1,28 @@
+import db from '../../db';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:User-repository');
+
+class PhotoRepository {
+  async upload(userId: number, path: string) {
+    const query = 'INSERT INTO pictures(user_id, file_path) VALUES($1, $2)';
+    db.query(query, [userId, path]);
+  }
+
+  async list(userId: number) {
+    const query = 'SELECT * FROM pictures WHERE user_id = $1';
+    return db.query(query, [userId]);
+  }
+
+  async getById(id: number) {
+    const query = 'SELECT * FROM pictures WHERE id = $1';
+    return db.query(query, [id]);
+  }
+
+  async delete(id: number) {
+    const query = 'DELETE FROM pictures WHERE id = $1';
+    return db.query(query, [id]);
+  }
+}
+
+export default new PhotoRepository();
