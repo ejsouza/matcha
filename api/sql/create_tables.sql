8eae3cc972ecf045dc9e9fc224c17cb4f5b2ef55
Eder De Souza <esouza@student.42.fr>
2021-10-19 05:25:16 +0200

    Update migration file

--- api/sql/create_tables.sql
+++ api/sql/create_tables.sql
@@ -1,20 +1,32 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE  users (
   id SERIAL PRIMARY KEY,
 	username VARCHAR(128) NOT NULL UNIQUE,
-  first_name VARCHAR(128) NOT NULL,
-	last_name VARCHAR(128) NOT NULL,
+  firstname VARCHAR(128) NOT NULL,
+	lastname VARCHAR(128) NOT NULL,
 	email VARCHAR(128) NOT NULL UNIQUE,
 	password VARCHAR(64) NOT NULL,
 	gender VARCHAR(16),
+	birthdate DATE,
 	sexual_orientation VARCHAR (16),
 	biography VARCHAR(255),
-	tags VARCHAR(250)
+	localisation POINT,
+	activated BOOLEAN DEFAULT FALSE,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- DROP TABLE IF EXISTS pictures;
 CREATE TABLE  pictures (
   id SERIAL PRIMARY KEY,
 	user_id SERIAL NOT NULL,
-	file_name VARCHAR(255),
-	file_data BYTEA NOT NULL,
+	file_path VARCHAR(255),
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+-- DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+	id INT,
+	user_id SERIAL NOT NULL,
+	name VARCHAR(64)
+)
