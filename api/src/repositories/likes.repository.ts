a1c6b92cdb0c18ea42cb420e867daae2b613c0ae
Eder De Souza <esouza@student.42.fr>
2021-10-21 11:47:26 +0200

    Create like repository

---
+++ api/src/repositories/likes.repository.ts
@@ -0,0 +1,26 @@
+import db from '../../db';
+import { CreateLikeDto, CreateDislikeDto } from '../dto/likes/create.like.dto';
+
+class LikeRepository {
+  async getUserLikes(userId: string) {
+    const query = 'SELECT * FROM likes WHERE user_id = $1';
+    return db.query(query, [userId]);
+  }
+
+  async getUserDislikes(userId: string) {
+    const query = 'SELECT * FROM dislikes WHERE user_id = $1';
+    return db.query(query, [userId]);
+  }
+
+  async addUserLike(like: CreateLikeDto) {
+    const query = 'INSERT INTO likes(user_id, liked_id) VALUES($1, $2)';
+    return db.query(query, [like.user_id, like.liked_id]);
+  }
+
+  async addUserDislike(dislike: CreateDislikeDto) {
+    const query = 'INSERT INTO dislikes(user_id, disliked_id) VALUES($1, $2)';
+    return db.query(query, [dislike.user_id, dislike.disliked_id]);
+  }
+}
+
+export default new LikeRepository();
