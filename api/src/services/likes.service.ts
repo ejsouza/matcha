08316e109bce7d975fc3c8c47783c633e2183014
Eder De Souza <esouza@student.42.fr>
2021-10-21 11:48:34 +0200

    Create like service

---
+++ api/src/services/likes.service.ts
@@ -0,0 +1,47 @@
+import likesRepository from '../repositories/likes.repository';
+import { CreateLikeDto, CreateDislikeDto } from '../dto/likes/create.like.dto';
+import usersService from './users.service';
+
+class LikeService {
+  async getUserLikes(userId: string) {
+    const res = await likesRepository.getUserLikes(userId);
+    const likes: CreateLikeDto[] = res.rows;
+    return likes;
+  }
+
+  async getUserDislikes(userId: string) {
+    const res = await likesRepository.getUserDislikes(userId);
+    const dislikes: CreateDislikeDto[] = res.rows;
+    return dislikes;
+  }
+
+  async addUserLike(userId: string, likedId: string) {
+    const like: CreateLikeDto = {
+      user_id: Number(userId),
+      liked_id: Number(likedId),
+    };
+    const res = await likesRepository.addUserLike(like);
+    const count = res.rowCount;
+    if (count > 0) {
+      console.log('Update increasePopularity()');
+      await usersService.increaseUserPopularity(Number(likedId));
+    }
+    return count;
+  }
+
+  async addUserDislike(userId: string, dislikedId: string) {
+    const dislike: CreateDislikeDto = {
+      user_id: Number(userId),
+      disliked_id: Number(dislikedId),
+    };
+    const res = await likesRepository.addUserDislike(dislike);
+    const count = res.rowCount;
+    if (count > 0) {
+      console.log('Update decreasePopularity()');
+      await usersService.decreaseUserPopularity(Number(dislikedId));
+    }
+    return count;
+  }
+}
+
+export default new LikeService();
