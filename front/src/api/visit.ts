903b3efc533eca03e0dbf46ea57d22246a368835
Eder De Souza <esouza@student.42.fr>
2021-10-24 18:24:44 +0200

    Create visit api calls

---
+++ front/src/api/visit.ts
@@ -0,0 +1,51 @@
+import { API_BASE_URL } from 'utils/config';
+import {
+  getUserTokenFromLocalStorage,
+  getUserIdFromLocalStorage,
+} from 'utils/user';
+
+/**
+ * @description PUT is used here becuse we should replace the visit if exist
+ * @param 			visiteeId
+ * @param 			visitorId
+ * @returns 		express Response
+ */
+const visitUserProfile = async (visiteeId: number, visitorId: number) => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/visits`, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getUserTokenFromLocalStorage()}`,
+      },
+      body: JSON.stringify({
+        visiteeId,
+        visitorId,
+      }),
+    });
+    return res;
+  } catch (err) {
+    console.log(`[catch error visitUserProfile()] ${err}`);
+    throw err;
+  }
+};
+
+const getVisits = async () => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/visits`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getUserTokenFromLocalStorage()}`,
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(`[catch error getVisits()] ${err}`);
+    throw err;
+  }
+};
+
+export { visitUserProfile, getVisits };
