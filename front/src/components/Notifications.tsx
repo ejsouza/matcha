a215b28c7468b131621437fbcb8b59c1ef9e4b42
Eder De Souza <esouza@student.42.fr>
2021-10-29 09:19:00 +0200

    Update notifications component

--- front/src/components/Notifications.tsx
+++ front/src/components/Notifications.tsx
@@ -5,13 +5,20 @@ import Bell from './Bell';
 import Loading from './Loading';
 import { getVisits } from 'api/visit';
 import { getUserMatches, UpdateUserInfoInterface } from 'api/user';
+import { getMessages } from 'api/message';
 import VisitCard from './VisitCard';
 import MatchesCard from './MatchesCard';
 import { SUCCESS } from 'utils/const';
+import DisplayMessageCard, { UserReceivedMessage } from './DisplayMessageCard';
 
 const Container = styled.div`
   padding-left: 16px;
   padding-right: 24px;
+`;
+
+const LoadingContainer = styled.div`
+  margin-top: 100px;
+  margin-left: 25%;
 `;
 
 const TitlePrev = styled.div`
@@ -20,7 +27,7 @@ const TitlePrev = styled.div`
   align-items: center;
   position: absolute;
   color: #868e96;
-  margin-left: -30px;
+  margin-left: -32px;
   height: 70px;
   width: 20%;
 `;
@@ -61,7 +68,7 @@ export interface VisitInterface {
   id: number;
   visitee_id: number;
   visitor_id: number;
-  seen_visit: boolean;
+  seen: boolean;
   visited_at: Date;
 }
 
@@ -69,7 +76,9 @@ const Notifications = (props: { notif: boolean }) => {
   const [index, setIndex] = useState(0);
   const [visits, setVisits] = useState<VisitInterface[]>();
   const [matches, setMatches] = useState<UpdateUserInfoInterface[]>();
+  const [messages, setMessages] = useState<UserReceivedMessage[]>();
   const [unseenVisits, setUnseenVisits] = useState(0);
+  const [unseenMessages, setUnseenMessages] = useState(0);
 
   const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
@@ -92,7 +101,7 @@ const Notifications = (props: { notif: boolean }) => {
     const visitors: VisitInterface[] = v.visits;
     let unseenV = 0;
     visitors.forEach((visitor) => {
-      if (!visitor.seen_visit) {
+      if (!visitor.seen) {
         unseenV++;
       }
     });
@@ -107,14 +116,30 @@ const Notifications = (props: { notif: boolean }) => {
     setMatches(matchedUsers);
   }, [props.notif]);
 
+  const getUserMessages = useCallback(async () => {
+    const res = await getMessages();
+    const m = await res.json();
+    const msgs: UserReceivedMessage[] = m.messages;
+    let unseenM = 0;
+    msgs.forEach((msg) => {
+      if (!msg.message.seen) {
+        unseenM++;
+      }
+    });
+    setUnseenMessages(unseenM);
+    setMessages(msgs);
+  }, [props.notif]);
+
   useEffect(() => {
     getUserVisits();
     getMatches();
-    console.log(`HERE >>> ${props.notif}`);
-  }, [getUserVisits, getMatches]);
+    getUserMessages();
+  }, [getUserVisits, getMatches, getUserMatches]);
 
-  return !visits || !matches?.length ? (
-    <Loading />
+  return !visits || !matches || !messages ? (
+    <LoadingContainer>
+      <Loading />
+    </LoadingContainer>
   ) : (
     <>
       <Container>
@@ -127,8 +152,12 @@ const Notifications = (props: { notif: boolean }) => {
             <WrapTitle>
               <TitlePrev>
                 {notificatons[prev]}&nbsp;
-                {prev === 0 && <Bell width="16" height="20" count={3} />}
-                {prev === 1 && <Bell width="16" height="20" count={2} />}
+                {prev === 0 && matches.length > 0 && (
+                  <Bell width="16" height="20" count={matches.length} />
+                )}
+                {prev === 1 && unseenMessages > 0 && (
+                  <Bell width="16" height="20" count={unseenMessages} />
+                )}
                 {prev === 2 && unseenVisits > 0 && (
                   <Bell width="16" height="20" count={unseenVisits} />
                 )}
@@ -139,8 +168,12 @@ const Notifications = (props: { notif: boolean }) => {
             <WrapTitle>
               <TitleNext>
                 {notificatons[next]}&nbsp;
-                {next === 0 && <Bell width="16" height="20" count={1} />}
-                {next === 1 && <Bell width="16" height="20" count={2} />}
+                {next === 0 && matches.length > 0 && (
+                  <Bell width="16" height="20" count={matches.length} />
+                )}
+                {next === 1 && unseenMessages > 0 && (
+                  <Bell width="16" height="20" count={unseenMessages} />
+                )}
                 {next === 2 && unseenVisits > 0 && (
                   <Bell width="16" height="20" count={unseenVisits} />
                 )}
@@ -151,11 +184,11 @@ const Notifications = (props: { notif: boolean }) => {
           <Carousel.Item>
             <Wrapper>
               <ActiveTitle>
-                {/* <hr /> */}
+                <hr />
                 <h6>Matches</h6>
               </ActiveTitle>
               {matches.length === 0 ? (
-                <p>You don't have any match, start swapping!</p>
+                <p>You didn't match anyone yet, start swapping!</p>
               ) : (
                 <MatchesCard matches={matches} />
               )}
@@ -164,24 +197,25 @@ const Notifications = (props: { notif: boolean }) => {
           <Carousel.Item>
             <Wrapper>
               <ActiveTitle>
-                {/* <hr /> */}
-
+                <hr />
                 <h6>Messages</h6>
               </ActiveTitle>
-              <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
+              {messages.length === 0 ? (
+                <p>You don't have any message!</p>
+              ) : (
+                <DisplayMessageCard userMessages={messages} />
+              )}
             </Wrapper>
           </Carousel.Item>
           <Carousel.Item>
             <Wrapper>
               <ActiveTitle>
-                {/* <hr /> */}
+                <hr />
 
                 <h6>Visits</h6>
               </ActiveTitle>
               {visits?.length === 0 ? (
-                <p>You don't have any visitor</p>
+                <p>You don't have any visitor!</p>
               ) : (
                 <>
                   <VisitCard visitors={visits} />
