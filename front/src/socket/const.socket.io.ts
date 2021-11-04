871acd22ac9745ff2a0c727dfbfa2564daf5befb
Eder De Souza <esouza@student.42.fr>
2021-11-04 08:33:03 +0100

    Create socket const

---
+++ front/src/socket/const.socket.io.ts
@@ -0,0 +1,13 @@
+const USER_CONNECTION = 'user:connection';
+
+const CHAT_SENT_MESSAGE = 'chat:sent:message';
+const CHAT_RECEIVED_MESSAGE = 'chat:received:message';
+
+const CONNECT_ERROR = 'connect_error';
+
+export {
+  USER_CONNECTION,
+  CHAT_SENT_MESSAGE,
+  CHAT_RECEIVED_MESSAGE,
+  CONNECT_ERROR,
+};
