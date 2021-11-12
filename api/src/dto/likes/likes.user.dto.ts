b511acf54294ab3c285a6fdcacaca4670e72b035
Eder De Souza <esouza@student.42.fr>
2021-11-12 08:56:20 +0100

    Create like dto

---
+++ api/src/dto/likes/likes.user.dto.ts
@@ -0,0 +1,45 @@
+import { CreateLikeDto, CreateDislikeDto } from './create.like.dto';
+
+/**
+ *
+ * @param likesMe
+ * @param likes
+ * @param dislikes
+ * @res  array[] of unique liked user
+ */
+export const MapUsersLikedMeDto = async (
+  likesMe: CreateLikeDto[],
+  likes: CreateLikeDto[],
+  dislikes: CreateDislikeDto[]
+) => {
+  const filteredLikes: CreateLikeDto[] = [];
+  /**
+   * Filter likes
+   * This map will check in the likesMe array for unique id
+   * if current user liked or disliked the id will be removed.
+   * The goal is to return only id that liked the current user
+   * that the current user didn't like or dislike yet.
+   */
+  likesMe.forEach((user) => {
+    /**
+     * Check if current user also liked who liked the user
+     */
+    const isMatch = likes.find((like) => like.liked_id === user.user_id);
+    /**
+     * Check if current user diliked who liked the user
+     */
+    const disliked = dislikes.find(
+      (dislike) => dislike.disliked_id === user.user_id
+    );
+
+    console.log(
+      `likesMe[${user.liked_id}] := ${user.user_id} :: isMatch := ${isMatch} :: disliked := ${disliked}`
+    );
+
+    if (!isMatch && !disliked) {
+      filteredLikes.push(user);
+    }
+  });
+  console.log(`<<---------- [END] -------->>`);
+  return filteredLikes;
+};
